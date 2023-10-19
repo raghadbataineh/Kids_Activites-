39d163980fdb5c8f7178a5c4cb3347d949c0f66b
Razan-Rjoub <ralrjoub6@gmail.com>
2023-10-19 20:28:44 +0300

    Razan

--- kids_react/src/components/pages/EventDetail/Sections/eventdetails.js
+++ kids_react/src/components/pages/EventDetail/Sections/eventdetails.js
@@ -4,16 +4,15 @@ import { useParams } from "react-router-dom";
 
 const Eventdetails = () => {
   const { id } = useParams(); // Assuming 'id' is the category ID
+
   const [data, setData] = useState([]);
-  const [organizer, setOrganizer] = useState([]);
+
   useEffect(() => {
     // Construct the URL with the 'id' (category ID)
     axios
       .get(`http://127.0.0.1:8000/api/EventDetails/${id}`)
       .then((response) => {
-        setData(response.data.events);
-        setOrganizer(response.data.organizer);
-        console.log(response.data);
+        setData(response.data);
       });
   }, [id]); // Include 'id' in the dependency array
 
@@ -55,10 +54,10 @@ const Eventdetails = () => {
             <div className="single_event_section_part">
               <div className="row justify-content-between">
                 <div
-                  className="col-lg-3 col-sm-6 wow fadeInUp"
+                  className="col-lg-4 col-sm-6 wow fadeInUp"
                   data-wow-delay=".3s"
                 >
-                  <div className="single_event_part event_bg_01" style={{ height: "415px" }}>
+                  <div className="single_event_part event_bg_01">
                     <div className="single_event_icon">
                       <img
                         src="https://html.droitlab.com/kidzo/img/event/list_event_7.png"
@@ -72,10 +71,10 @@ const Eventdetails = () => {
                   </div>
                 </div>
                 <div
-                  className="col-lg-3 col-sm-6 wow fadeInUp"
+                  className="col-lg-4 col-sm-6 wow fadeInUp"
                   data-wow-delay=".5s"
                 >
-                  <div className="single_event_part event_bg_02" style={{ height: "415px" }}>
+                  <div className="single_event_part event_bg_02">
                     <div className="single_event_icon">
                       <img
                         src="https://html.droitlab.com/kidzo/img/event/list_event_8.png"
@@ -90,10 +89,10 @@ const Eventdetails = () => {
                   </div>
                 </div>
                 <div
-                  className="col-lg-3 col-sm-6 wow fadeInUp"
+                  className="col-lg-4 col-sm-6 wow fadeInUp"
                   data-wow-delay=".5s"
                 >
-                  <div className="single_event_part event_bg_03" style={{ height: "415px" }}>
+                  <div className="single_event_part event_bg_03">
                     <div className="single_event_icon">
                       <img
                         src="https://html.droitlab.com/kidzo/img/program_img/program_img_06.png"
@@ -101,30 +100,11 @@ const Eventdetails = () => {
                         className="img-fluid"
                       />
                     </div>
-                    <h4>Event <br></br> price</h4>
-                    <p>{data.price} JOD <br></br> per kid</p>
+                    <h4>Event price</h4>
+                    <p>{data.price} JOD</p>
                     <br></br>
                   </div>
                 </div>
-                <div
-                  className="col-lg-3 col-sm-6 wow fadeInUp"
-                  data-wow-delay=".5s"
-                >
-                  <div className="single_event_part event_bg_02" style={{ height: "415px" }}>
-                    <div className="single_event_icon">
-                      <img
-                        src="https://html.droitlab.com/kidzo/img/event/list_event_8.png"
-                        alt="#"
-                        className="img-fluid"
-                      />
-                    </div>
-                    <h4>Event Organizer</h4>
-                    <p>
-                      {organizer.name}
-                    </p>
-                  </div>
-                </div>
-              
               </div>
             </div>
           </div>
