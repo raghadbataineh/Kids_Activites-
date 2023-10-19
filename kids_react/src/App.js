331954c201e40a864443d5dcaba19934b893736d
mohammad-alghzawi <mohd.a.ghzawi@gmail.com>
2023-10-19 22:32:37 +0300

    mohammad

--- kids_react/src/App.js
+++ kids_react/src/App.js
@@ -1,15 +1,27 @@
-import { Helmet } from 'react-helmet';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import Footer from './components/layouts/footer';
 import Navbar from './components/layouts/Navbar';
+import Register from './components/pages/login_register/register';
+import Login from './components/pages/login_register/login';
 
 function App() {
   return (
     <div className="App">
-       <Navbar />
-          
-      <Footer />
-   
+      <BrowserRouter>
+        <Navbar />
+        <Login />
+
+        <Routes>
+          <Route path="/register" component={Register} />
+          <Route path="/login" component={Login} />
+        </Routes>
+        <Footer />
+
+      </BrowserRouter>
+
     </div>
   );
 }
