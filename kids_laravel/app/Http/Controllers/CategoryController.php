4c9367164eaa3df7b3b7acd3a740e53adf281dfe
Rania-Taha <rania.taha105@gmail.com>
2023-10-18 21:18:20 -0700

    Rania

--- kids_laravel/app/Http/Controllers/CategoryController.php
+++ kids_laravel/app/Http/Controllers/CategoryController.php
@@ -14,7 +14,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $category = Category::all();
+        return response()->json($category);
     }
 
     /**
